23b23ec9c8de8e22ff3d16818f3dbfacc613c8cb
Bryan Overbey <bryanmorganoverbey@gmail.com>
2023-11-10 14:42:47 -0600

    load balancer with target group

--- ch-02/main.tf
+++ ch-02/main.tf
@@ -24,8 +24,12 @@ resource "aws_launch_configuration" "example" {
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier  = data.aws_subnets.default.ids
-  min_size             = 2
-  max_size             = 10
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
   tag {
     key                 = "Name"
     value               = "terraform-asg-example"
@@ -49,11 +53,9 @@ variable "server_port" {
   default     = 8080
 }
 
-# instead of having to manually poke around the console to find the IP address of the instance,
-# you can use Terraform’s output command to print the value of the public_ip attribute
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
 
 data "aws_vpc" "default" {
@@ -62,7 +64,82 @@ data "aws_vpc" "default" {
 
 data "aws_subnets" "default" {
   filter {
-    name   = "vpc_id"
+    name   = "vpc-id"
     values = [data.aws_vpc.default.id]
+  }
+}
+# the load balancer needs to have a security group defined to allow ingress and egress traffic
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+# This listener configures the ALB to listen on the default HTTP port, port 80, use HTTP as the protocol,
+# and send a simple 404 page as the default response for requests that don’t match any listener rules.
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+  # Allow inbound HTTP requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# the lb target group tells the load balancer to route traffic to the instances in the auto scaling group
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = 200
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+# the listener rule tells the load balancer to forward all requests to the target group
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
   }
 }
